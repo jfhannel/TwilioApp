e326b136b982636e899e9ebae27fe6e783c0c445
Jordan Hannel <jordie@simpleemotion.com>
2014-06-30 14:46:29 -0700

    Twilio stuff

---
+++ rest/call-request.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	// Include the Twilio PHP library
+	require '..\twilio-php-master\Services\Twilio.php';
+
+	// Twilio REST API version
+	$version = "2010-04-01";
+
+	// Set our Account SID and AuthToken
+	$sid = 'AC6ec38eec290d98eec8553e047397ba1c';
+	$token = 'c20d0079e3589de4677ed9553d24f46b';
+	
+	// A phone number you have previously validated with Twilio
+	$phonenumber = '+18058079069';
+	
+	// Instantiate a new Twilio Rest Client
+	$client = new Services_Twilio($sid, $token, $version);
+
+	try {
+		// Initiate a new outbound call
+		$call = $client->account->calls->create(
+			'+15033510708', // The number of the phone initiating the call
+			$phonenumber, // The number of the phone receiving call
+			'https://drive.google.com/file/d/0ByqdOBWF2BXUTlF6LWVsTjEyT28' // The URL Twilio will request when the call is answered
+		);
+		echo 'Started call: ' . $call->sid;
+	} catch (Exception $e) {
+		echo 'Error: ' . $e->getMessage();
+	}
+	?>
+	</body>
+	</html>
